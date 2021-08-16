6f501ed69c5bb868392f2b2ec5e731c20c4f1c2a
will <willian.urban@gmail.com>
2021-08-16 14:40:29 +0900

    fixed typos

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -8,18 +8,18 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFT is ERC721URIStorage {
     using Counters for Counters.Counter;
-    Counters.Conunter private _tokenIds;
-    address countractAddress;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
 
-    constrcutor(address marketPlaceAddress) ERC721("Metaverse Tokens", "MEET") {
+    constructor(address marketPlaceAddress) ERC721("Metaverse Tokens", "MEET") {
         contractAddress = marketPlaceAddress;
     }
 
-    function createToken(string memory tokenURI) public ruturns (uint) {
-        _tokenIds.increnet();
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
 
-        _mint(msg.senderm newItemid);
+        _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
         setApprovalForAll(contractAddress, true);
         
