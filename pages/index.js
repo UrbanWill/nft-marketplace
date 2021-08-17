830d1614fff42fb80fea8e9582dfef28ed6615eb
Will <willian@crowdcast.jp>
2021-08-17 20:19:50 +0900

    switch account test

--- pages/index.js
+++ pages/index.js
@@ -49,6 +49,8 @@ export default function Home() {
   console.log({ nfts });
   console.log({ loadingState });
 
+  // this is a test
+
   if (loadingState === "loaded" && !nfts.length)
     return <h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>;
 
