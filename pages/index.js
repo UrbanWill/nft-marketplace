d395a4a12fd0fba33eee7c2a1e32482549f1f3ab
UrbanWill <willian.urban@gmail.com>
2021-09-21 14:41:55 +0900

    added buyNft function

--- pages/index.js
+++ pages/index.js
@@ -46,6 +46,27 @@ export default function Home() {
     setLoadingState("loaded");
   }
 
+  async function buyNft(nft) {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer);
+
+    const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
+
+    const transaction = await contract.createMarketSale(
+      nftaddress,
+      nft.tokenId,
+      { value: price }
+    );
+
+    await transaction.wait();
+
+    loadNFTs();
+  }
+
   console.log({ nfts });
   console.log({ loadingState });
 
