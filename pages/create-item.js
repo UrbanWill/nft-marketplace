b9577431731ac1b86ed8507d1e5da2701c294921
UrbanWill <willian.urban@gmail.com>
2021-09-21 16:30:32 +0900

    removed commented code

--- pages/create-item.js
+++ pages/create-item.js
@@ -9,7 +9,6 @@ const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
 import { nftaddress, nftmarketaddress } from "../config";
 
 import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
-// import Market from "../artifacts/contracts/Market.sol/NFTMarket.json";
 import Market from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
 
 export default function CreateItem() {
