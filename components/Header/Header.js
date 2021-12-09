274e07ff3d85b4de68aa8d122ac2c9a4bc61f2b0
UrbanWill <willian.urban@gmail.com>
2021-12-09 16:18:51 +0900

    added isSelected to header item

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import { HEADER_ROUTES } from "../../utils/constants";
 
@@ -12,14 +13,27 @@ const ROUTES_LABELS = {
 };
 
 const Header = () => {
+  const { route } = useRouter();
+
+  const parsedRoute = route.substring(1);
+
   return (
     <nav className="border-b p-6">
       <p className="text-4xl font-bold">Metaverse Marketplace</p>
       <div className="flex mt-4">
-        {Object.values(HEADER_ROUTES).map((route) => {
+        {Object.values(HEADER_ROUTES).map((headerRoute) => {
+          const isSelected = headerRoute === parsedRoute;
           return (
-            <Link href={`/${route}`} key={route}>
-              <a className="mr-4 text-pink-500">{ROUTES_LABELS[route]}</a>
+            <Link href={`/${headerRoute}`} key={headerRoute}>
+              <a
+                className={`p-2 ${
+                  isSelected
+                    ? "bg-black rounded-md text-white"
+                    : "text-pink-500"
+                }`}
+              >
+                {ROUTES_LABELS[headerRoute]}
+              </a>
             </Link>
           );
         })}
