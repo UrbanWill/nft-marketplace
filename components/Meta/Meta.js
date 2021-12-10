828d1be8b906c4767161dd97c994560414b273bb
UrbanWill <willian.urban@gmail.com>
2021-12-10 12:18:16 +0900

    added favicon to meta

--- components/Meta/Meta.js
+++ components/Meta/Meta.js
@@ -14,7 +14,27 @@ const Meta = ({ title, keywords, description }) => {
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
       <meta charSet="utf-8" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <title>{title}</title>
     </Head>
   );
