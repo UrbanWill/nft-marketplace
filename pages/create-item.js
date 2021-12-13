255123dac86414e60e5faa34669c40267fdeeda5
UrbanWill <willian.urban@gmail.com>
2021-12-13 11:08:11 +0900

    added formik to create-item

--- pages/create-item.js
+++ pages/create-item.js
@@ -3,6 +3,7 @@ import { ethers } from "ethers";
 import { create as ipfsHttpClient } from "ipfs-http-client";
 import { useRouter } from "next/router";
 import Web3Modal from "web3modal";
+import { Formik, Form } from "formik";
 
 import { nftaddress, nftmarketaddress } from "../config";
 
@@ -13,12 +14,6 @@ const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
 
 export default function CreateItem() {
   const [fileUrl, setFileUrl] = useState(null);
-  // TODO: Move form state and validation to Formik and Yup
-  const [formInput, updateFormInput] = useState({
-    price: "",
-    name: "",
-    description: "",
-  });
 
   const router = useRouter();
 
@@ -37,7 +32,7 @@ export default function CreateItem() {
   };
 
   // TODO: Separete concerns, Creating an Item and listing an item should be two different functions
-  const createSale = async (url) => {
+  const createSale = async (url, salePrice) => {
     const web3Modal = new Web3Modal();
     const connection = await web3Modal.connect();
     const provider = new ethers.providers.Web3Provider(connection);
@@ -50,7 +45,7 @@ export default function CreateItem() {
     const event = tx.events[0];
     const value = event.args[2];
     const tokenId = value.toNumber();
-    const price = ethers.utils.parseUnits(formInput.price, "ether");
+    const price = ethers.utils.parseUnits(salePrice, "ether");
 
     /* then list the item for sale on the marketplace */
     contract = new ethers.Contract(nftmarketaddress, Market.abi, signer);
@@ -65,8 +60,8 @@ export default function CreateItem() {
   };
 
   // TODO: Clean this function and use added.path from onImageUpload
-  const createMarket = async () => {
-    const { name, description, price } = formInput;
+  const createMarket = async (values) => {
+    const { name, description, price } = values;
     if (!name || !description || !price || !fileUrl) return;
     /* first, upload to IPFS */
     const data = JSON.stringify({
@@ -78,60 +73,70 @@ export default function CreateItem() {
       const added = await client.add(data);
       const url = `https://ipfs.infura.io/ipfs/${added.path}`;
       /* after file is uploaded to IPFS, pass the URL to save it on Polygon */
-      createSale(url);
+      createSale(url, price);
     } catch (error) {
       console.log("Error uploading file: ", error);
     }
   };
 
+  const initialValues = {
+    name: "",
+    description: "",
+  };
+
+  const handleSubmit = async (values) => {
+    createMarket(values);
+  };
+
   return (
     <div className="flex justify-center">
-      <div className="w-1/2 flex flex-col pb-12">
-        <input
-          placeholder="Asset Name"
-          className="mt-8 border rounded p-4"
-          onChange={(e) =>
-            updateFormInput({ ...formInput, name: e.target.value })
-          }
-        />
-        <textarea
-          placeholder="Asset Description"
-          className="mt-2 border rounded p-4"
-          onChange={(e) =>
-            updateFormInput({ ...formInput, description: e.target.value })
-          }
-        />
-        <input
-          placeholder="Asset Price in Eth"
-          className="mt-2 border rounded p-4"
-          onChange={(e) =>
-            updateFormInput({ ...formInput, price: e.target.value })
-          }
-        />
-        <input
-          type="file"
-          name="Asset"
-          className="my-4"
-          onChange={onImageUpload}
-        />
-        {/* TODO: Use next image when refactoring this component */}
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        {fileUrl && (
-          <img
-            className="rounded mt-4"
-            width="350"
-            src={fileUrl}
-            alt="uploaded nft"
-          />
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        {({ values, handleChange }) => (
+          <Form className="w-1/2 flex flex-col pb-12">
+            <input
+              name="name"
+              placeholder="Asset Name"
+              className="mt-8 border rounded p-4"
+              onChange={handleChange}
+            />
+            <input
+              name="description"
+              placeholder="Asset description"
+              className="mt-8 border rounded p-4"
+              onChange={handleChange}
+            />
+            <input
+              name="price"
+              placeholder="Asset price in ETH"
+              className="mt-8 border rounded p-4"
+              onChange={handleChange}
+            />
+            <input
+              type="file"
+              name="asset"
+              className="my-4"
+              onChange={onImageUpload}
+            />
+            {fileUrl && (
+              // TODO: Use next image when refactoring this component
+              // eslint-disable-next-line @next/next/no-img-element
+              <img
+                className="rounded mt-4"
+                width="350"
+                src={fileUrl}
+                alt="uploaded nft"
+              />
+            )}
+            <button
+              type="submit"
+              className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg"
+            >
+              Create Digital Asset
+            </button>
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+          </Form>
         )}
-        <button
-          type="button"
-          onClick={createMarket}
-          className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg"
-        >
-          Create Digital Asset
-        </button>
-      </div>
+      </Formik>
     </div>
   );
 }
