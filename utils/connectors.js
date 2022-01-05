0975de6a43ebfd21e69371938293c2da362c917f
UrbanWill <willian.urban@gmail.com>
2022-01-05 11:37:24 +0900

    added matic testnet chanId to metamask connector

--- utils/connectors.js
+++ utils/connectors.js
@@ -2,7 +2,7 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 import { FortmaticConnector } from "@web3-react/fortmatic-connector";
 
 const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42, 1337],
+  supportedChainIds: [1, 3, 4, 5, 42, 1337, 80001],
 });
 
 const fortmatic = new FortmaticConnector({
