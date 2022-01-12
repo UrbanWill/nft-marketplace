d07d5776807777959b3a8f37ba5d64b11b7a2b8e
UrbanWill <willian.urban@gmail.com>
2022-01-12 19:13:51 +0900

    create contract function to getTokenIds, updated useGetOwnedNfts

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 import "hardhat/console.sol";
 
-contract NFT is ERC721URIStorage {
+contract NFT is ERC721URIStorage, ERC721Enumerable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     address contractAddress;
@@ -16,12 +17,52 @@ contract NFT is ERC721URIStorage {
         contractAddress = marketplaceAddress;
     }
 
-    function createToken(string memory tokenURI) public returns (uint) {
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+        function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+        function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    /**
+     * funtion to get all _owner nft tokens id
+     * @returns {_tokensOfOwner: uint[]} 
+     */
+    function getTokenIds(address _owner) public view returns (uint[] memory) {
+        uint[] memory _tokensOfOwner = new uint[](ERC721.balanceOf(_owner));
+        uint i;
+
+        for (i=0;i<ERC721.balanceOf(_owner);i++){
+            _tokensOfOwner[i] = ERC721Enumerable.tokenOfOwnerByIndex(_owner, i);
+        }
+        return (_tokensOfOwner);
+    }
+
+    function createToken(string memory IPFStokenURI) public returns (uint) {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
 
         _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+        _setTokenURI(newItemId, IPFStokenURI);
         setApprovalForAll(contractAddress, true);
         return newItemId;
     }
